a59c901bbdc7c527d548f0b265ee60c0ef0c9019
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-21 22:41:02 -0300

    Javascript - Básico: Operadores comparativos

--- aula/script.js
+++ aula/script.js
@@ -1,16 +1,9 @@
-var a = 10;
-
-a += 2;
-console.log("+=", a);
-
-a -= 2;
-console.log("+=", a);
-
-a *= 2;
-console.log("*=", a);
-
-a /= 2;
-console.log("/=", a);
-
-a %= 3;
-console.log("%=", a);
+console.log("Igual", 10 == "10");
+console.log("Estritamente Igual", 10 === "10");
+console.log("Diferente", 10 != "10");
+console.log("Estritamente Diferente", 10 !== "10");
+console.log("Maior", 10 > 10);
+console.log("Menor", 10 < 10);
+console.log("Menor Igual", 10 <= 10);
+console.log("Maior Igual", 10 >= 10);
+console.log("Maior Igual com tipos diferentes", 10 >= "10");
