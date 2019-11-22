25ad2840d9729030f1930c0b0543ff58859963e5
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-21 21:50:43 -0300

    Javascript - Básico: Tipos de dados

--- aula/script.js
+++ aula/script.js
@@ -1,4 +1,9 @@
-var a = "Hello World";
-console.log(a);
-a = "Hello World!!!";
-console.log(a);
+var a = 1;
+var b = "1";
+var c = null;
+var d = true;
+
+console.log("1" + "1");
+console.log(1 + 1);
+console.log(c);
+console.log(d);
