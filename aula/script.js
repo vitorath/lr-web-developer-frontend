8b33e60718b2bb826b76ad4b8d6e63daf5e7c201
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-23 14:37:34 -0300

    Javascript - Básico: Funções

--- aula/script.js
+++ aula/script.js
@@ -1,9 +1,22 @@
-var alunos = ["Vitor", "Jośe", "Ana", "Francine"];
-
-for (var i in alunos) {
-  console.log(i);
+function media(n1, n2) {
+  return (n1 + n2) / 2;
 }
 
-for (var aluno of alunos) {
-  console.log(aluno);
-}
+var med = media;
+
+console.log(med(10, 9));
+console.log(media(10, 9));
+console.log(media(7, 8));
+console.log(media(6.5, 8.3));
+
+var media = function(n1, n2) {
+  return (n1 + n2) / 2;
+};
+
+console.log("Função anônima " + media(10, 9));
+
+var media = (n1, n2) => {
+  return (n1 + n2) / 2;
+};
+
+console.log("Função anônima (arrow functions) " + media(10, 9));
