849d248a594553e776ad58e04f8bc6042490aca8
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-23 16:27:05 -0300

    Javascript - Básico: Objetos - Métodos

--- aula/script.js
+++ aula/script.js
@@ -1,9 +1,18 @@
-var aluno = { nome: "Vitor", nota: [7.5, 9.8] };
-aluno.matriculo = "12345";
-aluno["sobrenome"] = "Thomazini";
+function calcMedia() {
+  return (this.notas[0] + this.notas[1]) / 2;
+}
 
-var novaPropriedade = "status";
-aluno[novaPropriedade] = "Aprovado";
+var aluno = {
+  nome: "Vitor",
+  notas: [7.5, 9.8],
+  media: calcMedia
+};
 
-console.log(aluno.nome, aluno["notaA"]);
-console.log(aluno);
+var aluno2 = {
+  nome: "Franciane",
+  notas: [7.5, 9.8],
+  media: calcMedia
+};
+
+console.log(aluno, aluno.media());
+console.log(aluno2, aluno2.media());
