204ee08f9a707445aa81efd72d10e5385a3788a5
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-23 15:29:43 -0300

    Javascript - Básico: Objetos - Propriedades

--- aula/script.js
+++ aula/script.js
@@ -1,31 +1,9 @@
-var nomes = ["Vitor", "Franciane", "Ana"];
-var notasA = [7.8, 8.0, 8.5];
-var notasB = [9.0, 8.5, 5.0];
+var aluno = { nome: "Vitor", nota: [7.5, 9.8] };
+aluno.matriculo = "12345";
+aluno["sobrenome"] = "Thomazini";
 
-function media(notaA, notaB) {
-  return (notaA + notaB) / 2;
-}
+var novaPropriedade = "status";
+aluno[novaPropriedade] = "Aprovado";
 
-function passou(media) {
-  if (media > 7) {
-    return "Aprovado";
-  }
-  return "Reprovado";
-}
-
-for (var index in nomes) {
-  var notaA = notasA[index];
-  var notaB = notasB[index];
-  var notaMedia = media(notaA, notaB);
-  console.log(
-    nomes[index] +
-      " - " +
-      notaA +
-      " - " +
-      notaB +
-      " - " +
-      notaMedia +
-      " - " +
-      passou(notaMedia)
-  );
-}
+console.log(aluno.nome, aluno["notaA"]);
+console.log(aluno);
