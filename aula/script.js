57c532bd14a67486e625a4307bc7041762761ea9
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-23 02:17:44 -0300

    Javascript - Básico: Condicionais - Switch

--- aula/script.js
+++ aula/script.js
@@ -1,12 +1,29 @@
-var idade = 17;
+var nota1 = 5.0;
+var nota2 = 5.8;
 
-if (idade >= 35 && idade <= 70) {
-  console.log("Pode");
-  console.log("Qual seu pedido");
-} else if (idade >= 18) {
-  console.log("Pode");
-  console.log("Mostre a identidade");
+var media = (nota1 + nota2) / 2;
+var conceito = "";
+
+if (media >= 8) {
+  conceito = "Ótimo";
+} else if (media >= 6.5) {
+  conceito = "Bom";
 } else {
-  console.log("Não pode");
-  console.log("Volte futuramente");
+  conceito = "Regular";
+}
+console.log("Média: " + media + " - Conceito: " + conceito);
+
+switch (conceito) {
+  case "Ótimo":
+    console.log("Parabéns, você é um ótimo aluno");
+    break;
+  case "Bom":
+    console.log("Você é quase perfeito");
+    break;
+  case "Regular":
+    console.log("Estudo um pouco mais");
+    break;
+  default:
+    console.log("Houve algum erro");
+    break;
 }
