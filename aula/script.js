a6fb4c1d6cdf6afc803ef80fde545482574214be
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-24 13:13:14 -0300

    Javascript - Básico: Window

--- aula/script.js
+++ aula/script.js
@@ -1,14 +1,5 @@
-var d = new Date("sep 05 2017 22:30");
+console.log(window);
+window.console.log("Teste");
+console.log("Teste");
 
-d = new Date("07 05 2017  22:30");
-console.log(d);
-
-d = new Date("12/25/2018");
-console.log(d);
-
-var dias = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
-
-console.log(d.getFullYear());
-console.log(d.getDate());
-console.log(dias[d.getDay()]);
-console.log(d.getTime());
+// Métodos de window podem ser chamados sem a identificação do objeto window
