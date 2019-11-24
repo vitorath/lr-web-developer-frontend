f5979460990672d5197dfae33c415f4580bc3b30
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-24 13:55:56 -0300

    Javascript - Intermediário: Document

--- aula/script.js
+++ aula/script.js
@@ -1,5 +1,6 @@
-console.log(window);
-window.console.log("Teste");
-console.log("Teste");
-
-// Métodos de window podem ser chamados sem a identificação do objeto window
+console.log(document);
+console.log(document.body);
+console.log(document.body.innerHTML);
+document.body.innerHTML =
+  "<h1>Este título foi inserido no javascript</h1>" + document.body.innerHTML;
+console.log(document.body.innerHTML);
