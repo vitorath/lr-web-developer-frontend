18514dd7407963ebc939dd5f8ed994b72e523a55
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-26 19:24:56 -0300

    Javascript - Intermediário: Modificando elementos

--- aula/script.js
+++ aula/script.js
@@ -1,12 +1,23 @@
 let p1 = document.getElementById("p1");
+p1.innerText = "<h3>Tem h3 dento p</h3>";
+
+let p2 = document.getElementById("p2");
+p2.textContent = "<h3>Tem h3 dento p</h3>";
+
+let p3 = document.getElementById("p3");
+p3.innerHTML = "<h3>Tem h3 dento p</h3>";
+
+p1.className = "";
+p1.style = "color: blue;";
+
+let meuAttr = p1.getAttribute("meuAttr");
+console.log(meuAttr);
 console.log(p1);
 
-let paragrafos = document.getElementsByClassName("paragrafo");
-console.log(paragrafos);
+p1.setAttribute("meuAttr", "Outro valor");
+console.log(p1);
 
-for (let p of paragrafos) {
-  console.log(p);
-}
+p1.setAttribute("novoAttr", "Nobo valor");
+console.log(p1);
 
-let tp = document.getElementsByTagName("p");
-console.log(tp);
+p1.style.backgroundColor = "red";
