6307d8bc49633439218a49ce4eda0d35b9a16639
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-30 15:45:15 -0300

    Javascript - Intermediário: Funções run vs ref

--- aula/script.js
+++ aula/script.js
@@ -1,9 +1,7 @@
-let t1 = document.getElementById("t1");
-let t2 = document.getElementById("t2");
-
-t1.onclick = colorcarEmMaiusculo;
-t2.onclick = colorcarEmMaiusculo;
-
-function colorcarEmMaiusculo() {
-  this.innerHTML = this.innerHTML.toUpperCase();
+function ret3() {
+  console.log(3);
+  return 5;
 }
+
+console.log(ret3());
+console.log(ret3);
