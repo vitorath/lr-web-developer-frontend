04d32be5b0aa38f5e34249ef1a3b3b55dbbd6440
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-11-30 13:34:17 -0300

    Javascript - Intermediário: Eventos DOM p2

--- aula/script.js
+++ aula/script.js
@@ -1,19 +1,7 @@
-function clickH1() {
-  console.log("Executando função");
-  this.style.color = "red";
+function onOver(elemento) {
+  elemento.style.backgroundColor = "red";
 }
 
-function changeInput(input) {
-  let h1 = document.getElementsByTagName("h1")[0];
-  h1.innerText = input.value;
-}
-
-function toggleH1() {
-  console.log("tes");
-  let h1 = document.getElementsByTagName("h1")[0];
-  if (h1.style.display === "none") {
-    h1.style.display = "block";
-  } else {
-    h1.style.display = "none";
-  }
+function onOut(elemento) {
+  elemento.style.backgroundColor = "blueviolet";
 }
