3fd3bf57f94cebd640df27ad3c0a0e5b9b5e4ddb
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-12-02 23:27:15 -0300

    Javascript - Intermediário:  event -  html vs js

--- aula/script.js
+++ aula/script.js
@@ -1,7 +1,17 @@
-function ret3() {
-  console.log(3);
-  return 5;
+function clickElement(e) {
+  console.log("element");
+  console.log(this);
+  console.log(e);
 }
 
-console.log(ret3());
-console.log(ret3);
+function clickJavascript(e) {
+  console.log("javascript");
+  console.log(this);
+  console.log(e);
+  console.log(e.target);
+}
+
+window.onload = function() {
+  let h1 = document.getElementsByTagName("h1")[0];
+  h1.addEventListener("click", clickJavascript);
+};
